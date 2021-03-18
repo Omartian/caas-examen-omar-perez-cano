e3400a97f1c3dec74de565414ac82bb9608f479b
omar-perez-cano-itlighten <omartian21@gmail.com>
2021-03-18 00:10:48 -0600

    Por fin consegui un request (╯°□°）╯︵ ┻━┻

--- index.js
+++ index.js
@@ -7,38 +7,11 @@ const fetch = require('node-fetch');
 
 const server = http.createServer((req, res) => {
     if (req.method === 'POST') {
-        let body = '';
-        req.on('data', chunk => {
-        body += chunk.toString(); // convert Buffer to string
+        collectRequestData(req, result => {
+            console.log(result);
+            res.end(`Parsed data belonging to ${result.text}`);
         });
-        req.on('end', () => {
-        console.log(parse(body));
-        data = parse(body);
-        console.log(parse(data.text));
-
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json'},
-            body: data
-        }
-        
-        const res = await fetch('https://paas-examen-omar-perez-cano.us-south.cf.appdomain.cloud/get-tone', options);
-        
-
-        res.end(`
-        <!doctype html>
-        <html>
-        <body>
-            <form action="/" method="post">
-                <input type="text" name="text" /><br />
-                <button>Enviar</button>
-            </form>
-        </body>
-        </html>
-      `);
-    });
-    }
-    else {
+    } else {
       res.end(`
         <!doctype html>
         <html>
@@ -56,3 +29,34 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
 });
+
+function collectRequestData(request, callback) {
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if(request.headers['content-type'] === FORM_URLENCODED) {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString(); // convert Buffer to string
+        });
+        request.on('end', async () => {
+            callback(parse(body));
+            console.log(parse(body));
+            data = parse(body);
+            console.log(parse(data.text));
+
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        }
+        
+        res = await fetch('https://paas-examen-omar-perez-cano.us-south.cf.appdomain.cloud/get-tone', options);
+
+        console.log(res.body);
+        });
+        } else {
+            callback(null);
+        }
+        }
+
+
+        
