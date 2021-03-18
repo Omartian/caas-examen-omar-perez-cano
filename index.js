8d0c2842a2bb89f7961abccdca05272d28979c16
omar-perez-cano-itlighten <omartian21@gmail.com>
2021-03-17 22:48:52 -0600

    formulario de envío de texto

--- index.js
+++ index.js
@@ -2,19 +2,42 @@ const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+const { parse } = require('querystring');
 
 const server = http.createServer((req, res) => {
-    res.end(`
+    if (req.method === 'POST') {
+        let body = '';
+        req.on('data', chunk => {
+        body += chunk.toString(); // convert Buffer to string
+        });
+        req.on('end', () => {
+        console.log(parse(body));
+        res.end(`
         <!doctype html>
         <html>
         <body>
             <form action="/" method="post">
-                <input type="text" name="fname" /><br />
+                <input type="text" name="text" /><br />
                 <button>Enviar</button>
             </form>
         </body>
         </html>
-    `);
+      `);
+    });
+    }
+    else {
+      res.end(`
+        <!doctype html>
+        <html>
+        <body>
+            <form action="/" method="post">
+                <input type="text" name="text" /><br />
+                <button>Enviar</button>
+            </form>
+        </body>
+        </html>
+      `);
+    }
 });
 
 server.listen(port, hostname, () => {
